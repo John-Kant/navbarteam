8209ab8fe2d78f9c9db9b6eb3aa4aa28c96a06f3
John Gomez <johngomez5541@yahoo.com>
2021-01-24 20:36:18 +0100

    second commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'HOME',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'ABOUT',
+  },
+  {
+    id: 3,
+    url: '/moving',
+    text: 'MOVING AROUND',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'CONTACT',
+  }
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://en.wikipedia.org/wiki/Senegal',
+    icon: <FaMapMarkerAlt />,
+    target: "_blank"
+  }
+];
